ab61231e0aecb8ab735b5776bc151154a316ef66
Darius <dariusford15@gmail.com>
2023-02-20 20:24:06 -0500

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,68 @@
-import logo from './logo.svg';
 import './App.css';
+import React ,{useState, useEffect} from'react'
+import ContactCard from './components/ContactCard';
 
 function App() {
+  const title = "Contact List";
+  {/*const contacts = [
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Jenny Han",
+      email: "jennyhan@notreal.com",
+      age: 25
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Paul Jones",
+      email: "paul.jones@notreal.com",
+      age: 33
+    },
+    {
+      avatarUrl: "https://via.placeholder.com/150",
+      name: "Peter Pan",
+      email: "peter.pan@notreal.com",
+      age: 100
+    }
+]*/}
+const [results,setResults] = useState([])
+
+useEffect(()=>{
+fetch("https://randomuser.me/api/?results=10")
+//pass in a function that takes a response
+.then(response => response.json())
+//take the data that we get from the json response and output it in our console
+.then(data => {
+  console.log(data)
+  setResults(data.results)
+  })
+}, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <h1>{title}</h1>
+      {results.map((result, index)=>{
+        return(
+        <ContactCard 
+        key={index}
+        name={result.name.first}
+        email={result.email}
+        avatarUrl={result.picture.large}
+        age={result.dob.age}/>
+        )
+      })}
+      
+     {/*} <ContactCard 
+        name="Paul Jones" 
+        email="paul.jones@notreal.com"
+        avatarUrl="https://via.placeholder.com/150"
+        age={25}/>
+        <ContactCard 
+        name="Peter Pan" 
+        email="peter.pan@notreal.com"
+        avatarUrl="https://via.placeholder.com/150"
+        age={100}/>
+    */}
+      
+      </div>
   );
 }
 
